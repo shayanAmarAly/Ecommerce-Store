9ecf7a1b9353bff72dca72aa8ee0374e71f9285e
shayanAmarAli <shayanamaralie@gmail.com>
2023-07-27 15:07:53 -0700

    third commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/", '/api/cart'],
+    publicRoutes: ["/", '/api/cart', "/male", '/female', '/kids', 'allproducts', '/productDetail/[slugs]'],
 });
 
 export const config = {
